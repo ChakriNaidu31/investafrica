b06ea8f2669c93a581442c467118635cc4914a98
ChakriNaidu31 <chakradharallamm@gmail.com>
2024-08-12 23:06:35 +0530

    routing signup

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,7 +4,6 @@ import globe from "../../assets/globe.png";
 
 function Login() {
   return (
-
     <div className="container-fluid">
       <div className="row">
         <nav class="navbar navbar-expand-lg">
@@ -31,53 +30,41 @@ function Login() {
         </nav>
 
         <div className="col-12 p-0">
-      <body class="login-body d-flex align-items-center py-4 bg-body-tertiary">
-    
-
-    
-<main class="form-signin w-100 m-auto">
-  <form>
-    
-    <h1 class="mb-3 login-head">Sign In</h1>
-
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label login-label">Email or username</label>
-    <input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or username"/>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label login-label">Password</label>
-    <span className="float-end login-fg">Forgot password</span>
-    <input type="password" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password"/>
-  </div>
-  </form>
-
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
-    </div>
-    <button class="btn py-2 px-4 login-btn mt-2" type="submit">Log in</button>
-    
-  </form>
-  <div className="text-center mt-3">
-    <p className="login-inv">New to Invest Africa?</p>
-    <button class="btn login-register py-2 px-4" type="submit">Register for an account</button>
-
-  </div>
-  <div className="text-start mt-5">
-    <span className="login-credit">Help?</span>
-    <span className="login-credit ms-4">About us</span>
-  </div>
-</main>
-
-    
-
-</body>
+          <body class="signin-body d-flex align-items-center py-4 bg-body-tertiary">
+            <main class="form-signin w-100 m-auto">
+              <form>
+                <h1 class="mb-3 login-head">Sign In</h1>
+                <form>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label login-label">Email or username</label>
+                    <input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or username" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label login-label">Password</label>
+                    <span className="float-end login-fg">Forgot password</span>
+                    <input type="password" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password" />
+                  </div>
+                </form>
+                <div class="form-check text-start my-3">
+                  <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Remember me
+                  </label>
+                </div>
+                <button class="btn py-2 px-4 login-btn mt-2" type="submit">Log in</button>
+              </form>
+              <div className="text-center mt-3">
+                <p className="login-inv">New to Invest Africa?</p>
+                <button class="btn login-register py-2 px-4" type="submit">Register for an account</button>
+              </div>
+              <div className="text-start mt-5">
+                <span className="login-credit">Help?</span>
+                <span className="login-credit ms-4">About us</span>
+              </div>
+            </main>
+          </body>
+        </div>
       </div>
-      </div>
-     
     </div>
   )
 }
